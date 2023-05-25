3026f3517258eaa6b648414296e3234f88d3a5d7
AK <andre.k.kollmar@gmx.de>
2023-05-25 09:27:23 +0200

    Update setup

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	root: 'src',
-	assetsInclude: ['./assets/**/*.*'],
+	// assetsInclude: ['./assets/**/*.*'],
 	build: {
 		emptyOutDir: true,
 		outDir: '../dist',
